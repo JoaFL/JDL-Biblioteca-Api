dfcb9192ee98cd3ee9fd7787168e92d6621fb880
Joaquimfl8 <160670206+Joaquimfl8@users.noreply.github.com>
2026-03-18 15:49:29 -0300

    Primeiro commit

---
+++ src/main/java/JDL/Biblioteca/SaudacaoController.java
@@ -0,0 +1,16 @@
+package JDL.Biblioteca;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SaudacaoController {
+    
+    @GetMapping("/")
+    public String dizerOla(@RequestParam(value = "nome", defaultValue = "Mundo") String nome) {
+        return "Olá" + nome + "!";
+    }
+}
